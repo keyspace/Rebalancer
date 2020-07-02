5c5619cd0da27214b5295d97058d360e0abbf1c5
keyspace <keyspaceksp@gmail.com>
2020-07-02 17:58:15 +0000

    Debug-print block names to log.

---
+++ Data/Scripts/Rebalancer/Session.cs
@@ -0,0 +1,40 @@
+﻿using Sandbox.Definitions;
+using System.Collections.Generic;
+using System.Linq;
+using VRage.Game.Components;
+using VRage.Utils;
+
+namespace Keyspace.Rebalancer
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Rebalancer_Session : MySessionComponentBase
+    {
+        public static Rebalancer_Session Instance;
+
+        public override void LoadData()
+        {
+            Instance = this;
+        }
+
+        public override void BeforeStart()
+        {
+            var definitions = MyDefinitionManager.Static.GetAllDefinitions();
+            var cubeBlockDefinitions = definitions.ToList().Where(d => d is MyCubeBlockDefinition);
+
+            MyLog.Default.WriteLineAndConsole("XXX DEFINITIONS:");
+            foreach (var cbd in cubeBlockDefinitions)
+            {
+                MyLog.Default.WriteLineAndConsole(cbd.DisplayNameText);
+            }
+        }
+
+        protected override void UnloadData()
+        {
+            Instance = null;
+        }
+
+        //public override void UpdateAfterSimulation()
+        //{
+        //}
+    }
+}
